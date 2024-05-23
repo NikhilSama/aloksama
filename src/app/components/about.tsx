caea3ae7178aa13b626284b68d1d57e051aed01b
Nikhil Sama <nikhilsama@gmail.com>
2024-05-23 20:47:02 +0530

    first stab

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -11,23 +11,23 @@ export default function About() {
         Nvidia, TikTok, Uber, T-Mobile, Alibaba and WeWork.
         <br />
         <br />
-        The Money Trap is Sama’s thrilling, stranger-than-fiction personal
-        odyssey featuring his experiences alongside SoftBank’s iconic founder,
-        Masayoshi Son, a visionary maverick who wants to be remembered as “the
-        crazy guy who bet on the future” and whose mission is “happiness for
-        everyone.” Sama takes the reader on a wild journey as he consorts with
-        A-list CEOs and heads of state, and negotiates mega-deals on a
-        Gulfstream jet, Son’s sanctuary with its exquisite Japanese garden, and
-        a waterside restaurant in the Turkish Riviera—all while contending with
-        a mysterious dark-arts smear campaign that takes a toll on his private
-        life.
+        The Money Trap is Sama&apos;s thrilling, stranger-than-fiction personal
+        odyssey featuring his experiences alongside SoftBank&apos;s iconic
+        founder, Masayoshi Son, a visionary maverick who wants to be remembered
+        as “the crazy guy who bet on the future” and whose mission is “happiness
+        for everyone.” Sama takes the reader on a wild journey as he consorts
+        with A-list CEOs and heads of state, and negotiates mega-deals on a
+        Gulfstream jet, Son&apos;s sanctuary with its exquisite Japanese garden,
+        and a waterside restaurant in the Turkish Riviera—all while contending
+        with a mysterious dark-arts smear campaign that takes a toll on his
+        private life.
         <br />
         <br />
         This fascinating and humorous saga provides a unique insider perspective
         on the insanity of high finance and venture investing. Written with
         self-deprecating wit, unflinching honesty and searing introspection, The
         Money Trap is ultimately a morality tale: in life, as in technology
-        investing, more money isn’t always the answer.
+        investing, more money isn&apos;t always the answer.
       </Typography>
     </Box>
   );
